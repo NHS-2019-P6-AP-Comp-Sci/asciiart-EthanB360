c8bc380aa66542ae2216a0fd8fc937c1b0db49f4
s-baie <s-baie@53S500042463.it.bsd405.org>
2019-09-20 13:24:49 -0700

    Turning in project 1

--- ASCIIArt/src/ASCIIArt.java
+++ ASCIIArt/src/ASCIIArt.java
@@ -1,27 +1,70 @@
 /**
  * Unit 1 Project
  * 
- * In this project you have the opportunity to create a piece of artwork using ASCII 
- * (American Standard Code for Information Interchange). Using the already created file 
- * named `ASCIIArt.java` write a program that outputs your artwork to the console. 
- * The goal of this project is to use ONLY what we have learned in Unit 1 to create 
- * anything you would like so be creative (and school appropriate). Artwork must also have a caption describing what it's depicting.  
+ * In this project you have the opportunity to create a piece of artwork using
+ * ASCII (American Standard Code for Information Interchange). Using the already
+ * created file named `ASCIIArt.java` write a program that outputs your artwork
+ * to the console. The goal of this project is to use ONLY what we have learned
+ * in Unit 1 to create anything you would like so be creative (and school
+ * appropriate). Artwork must also have a caption describing what it's
+ * depicting.
  * 
- * For those of you who have dabbled in coding before, try to contain your code to 
- * just what we have discussed in this unit.  
+ * For those of you who have dabbled in coding before, try to contain your code
+ * to just what we have discussed in this unit.
  * 
- * @author Mr. Rasmussen 
+ * @author Mr. Rasmussen
  *
  */
 
 // YOU CODE HERE :) 
 
 public class ASCIIArt {
-	public static void main (String [] args) {
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
+	public static void printEars() {
+		System.out.print(" ()");
+	}
+
+	public static void printHead() {
+		System.out.println("    (*   *)");
+		System.out.println("    \\\\   //");
+		System.out.println("    -- ^ --");
+		System.out.println("    //   \\\\");
+	}
+
+	public static void printStomach() {
+		System.out.println("   (      )");
+	}
+
+	public static void printArms() {
+		System.out.println("$- |      | -$");
+	}
+
+	public static void printLegs() {
+		System.out.print("   | |");
+	}
+
+	public static void printFoot() {
+		System.out.print("  (--)");
+	}
+
+	public static void main(String[] args) {
+		// MY RABBIT
+		System.out.println("This is my phat rabbit");
+		System.out.print("    ");
+		printEars();
+		printEars();
+		System.out.println();
+		printHead();
+		printStomach();
+		printArms();
+		printStomach();
+		printLegs();
+		printLegs();
+		System.out.println();
+		System.out.println("    \\\\  //");
+		printFoot();
+		printFoot();
+		System.out.println();
+		System.out.println("The fattest rabbit alive with skinny arms and MASSIVE whiskers");
+
 	}
 }
